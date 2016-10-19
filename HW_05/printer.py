0d89163505819085336c46f34a7042d739339c76
GrigoryBartosh <grishabartosh@yandex.ru>
2016-10-19 16:12:54 +0300

    printer fix print None + print intent + if len(x) to if x

--- HW_05/printer.py
+++ HW_05/printer.py
@@ -2,6 +2,8 @@ from yat.model import *
 
 class PrettyPrinter:
 	
+	indent_type = "    ";
+
 	def __init__(self):
 		self.flag = False
 
@@ -16,43 +18,41 @@ class PrettyPrinter:
 			print(";")
 
 	def visitNumber(self, number, sp_cnt):
-		print("    "*sp_cnt + str(number.value), end="")
+		print(indent_type*sp_cnt + str(number.value), end="")
 
 	def visitFunctionDefinition(self, foo, sp_cnt):
-		print("    "*sp_cnt + "def " + foo.name + " (" + ", ".join(foo.function.args) + ") {")
+		print(indent_type*sp_cnt + "def " + foo.name + " (" + ", ".join(foo.function.args) + ") {")
 		for cmnd in foo.function.body:
 			self.visit(cmnd, sp_cnt+1)
 			print(";")
-		print("    "*sp_cnt+"}", end="")
+		print(indent_type*sp_cnt+"}", end="")
 
 	def visitConditional(self, conditional, sp_cnt):
-		print("    "*sp_cnt + "if (", end="")
+		print(indent_type*sp_cnt + "if (", end="")
 		self.visit(conditional.condtion, 0)
 		print(") {")
 		for x in conditional.if_true:
 			self.visit(x, sp_cnt+1)
 			print(";")
 		if conditional.if_false != None:
-			print("    "*sp_cnt + "} else {")
+			print(indent_type*sp_cnt + "} else {")
 			for x in conditional.if_false:
 				self.visit(x, sp_cnt+1)
 				print(";")
-		else:
-			print(conditional.if_false)
-		print("    "*sp_cnt+"}", end="")
+		print(indent_type*sp_cnt+"}", end="")
 
 	def visitPrint(self, prnt, sp_cnt):
-		print("    "*sp_cnt + "print(", end="")
+		print(indent_type*sp_cnt + "print(", end="")
 		self.visit(prnt.expr, 0)
 		print(")", end="")
 
 	def visitRead(self, read, sp_cnt):
-		print("    "*sp_cnt + "read(" + read.name + ")", end="")
+		print(indent_type*sp_cnt + "read(" + read.name + ")", end="")
 
 	def visitFunctionCall(self, foo, sp_cnt):
 		self.visit(foo.fun_expr, sp_cnt)
 		print("(", end="")
-		if len(foo.args):
+		if foo.args:
 			for x in foo.args[:-1]:
 				self.visit(x, 0)
 				print(", ", end="")
@@ -60,10 +60,10 @@ class PrettyPrinter:
 		print(")", end="")
 
 	def visitReference(self, reference, sp_cnt):
-		print("    "*sp_cnt + reference.name, end="")
+		print(indent_type*sp_cnt + reference.name, end="")
 
 	def visitBinaryOperation(self, binary, sp_cnt):
-		print("    "*sp_cnt + "(", end="")
+		print(indent_type*sp_cnt + "(", end="")
 		self.visit(binary.lhs, 0)
 		print(") " + binary.op + " (", end="")
 		self.visit(binary.rhs, 0)
